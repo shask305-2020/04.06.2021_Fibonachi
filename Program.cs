6d6bafb673c08182579fed38e535524066bd25a3
Teacher <shask304@yandex.ru>
2021-06-04 00:03:20 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._06._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите количество чисел ряда Фибоначчи: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] fib = new int[n];
+                for (int i = 0; i < n + 1; i++)
+                {
+
+                }
+                Console.WriteLine(Fib(n));
+            }
+        }
+
+        static int Fib(int n)
+        {
+            if (n <= 2)
+                return 1;
+            else
+                return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
